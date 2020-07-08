c7fa27e4f98dcd8c8065b74880b0967e12c0ff1f
AndreySolano <ysolano@unal.edu.co>
2020-07-08 11:38:11 -0500

    Arreglos ultimahora

--- PaintPoseNet/PaintPoseNet.js
+++ PaintPoseNet/PaintPoseNet.js
@@ -36,7 +36,7 @@ let sel_quad=0;
 let sel_square=0;
 let sel_triangle=0;
 
-function preload(){
+function preload() {
   imgBorr = loadImage('data/borrador.png');
   imgGuar = loadImage('data/guardar.png');
   imgX = loadImage('data/x.png');
@@ -58,30 +58,30 @@ function setup() {
   canvas1 = createGraphics(640, 480);
   canvas2 = createGraphics(640, 480);
   canvas3 = createGraphics(640, 480);
-  
+
   canvas0.background(255); //**
   canvas1.background(100);
   canvas2.background(0); 
   canvas3.background(180);
 
   rect(0, 0, 100, 100);
-  
-  boton1 = new Boton(455,20,25,25,245,64,64);
-  boton2 = new Boton(420,20,25,25,245,239,49);
-  boton3 = new Boton(385,20,25,25,50,100,200);
-  boton4 = new Boton(350,20,25,25,0,255,0);
-  boton5 = new Boton(315,20,25,25,113,36,178);
-  boton6 = new Boton(280,20,25,25,0,0,0);
+
+  boton1 = new Boton(455, 20, 25, 25, 245, 64, 64);
+  boton2 = new Boton(420, 20, 25, 25, 245, 239, 49);
+  boton3 = new Boton(385, 20, 25, 25, 50, 100, 200);
+  boton4 = new Boton(350, 20, 25, 25, 0, 255, 0);
+  boton5 = new Boton(315, 20, 25, 25, 113, 36, 178);
+  boton6 = new Boton(280, 20, 25, 25, 0, 0, 0);
   botonImg1 = new BotonImg(490, 20, 25, 25, 255, 255, 255, imgBorr);
   guardar = new Guarda(605, 245, 25, 25, 255, 255, 255, imgGuar, c);
   reset = new Reset(605, 210, 25, 25, 255, 240, 240, imgX, c);  
-  botonCanvas0 = new BotonCanvas(605,70,25,25,245,64,64,0);
-  botonCanvas1 = new BotonCanvas(605,105,25,25,245,239,49,1);
-  botonCanvas2 = new BotonCanvas(605,140,25,25,50,100,200,2);
-  botonCanvas3 = new BotonCanvas(605,175,25,25,0,255,0,3);
-  botonFiltro0 = new BotonFiltro(525,20,25,25,255,255,255);
-  botonFiltro1 = new BotonFiltro(560,20,25,25,245,239,49);
-  botonFiltro2 = new BotonFiltro(595,20,25,25,50,220,100);
+  botonCanvas0 = new BotonCanvas(605, 70, 25, 25, 245, 64, 64, 0);
+  botonCanvas1 = new BotonCanvas(605, 105, 25, 25, 245, 239, 49, 1);
+  botonCanvas2 = new BotonCanvas(605, 140, 25, 25, 50, 100, 200, 2);
+  botonCanvas3 = new BotonCanvas(605, 175, 25, 25, 0, 255, 0, 3);
+  botonFiltro0 = new BotonFiltro(525, 20, 25, 25, 255, 255, 255);
+  botonFiltro1 = new BotonFiltro(560, 20, 25, 25, 245, 239, 49);
+  botonFiltro2 = new BotonFiltro(595, 20, 25, 25, 50, 220, 100);
 }
 
 function modelReady() {
@@ -121,51 +121,83 @@ function gotPoses(poses) {
 
 
 function draw() {
-  
-  background(255,255,255);
-  
+
+  background(255, 255, 255);
+
   translate(video.width, 0);
   scale(-1, 1);
 
   //image(video, 0, 0, video.width, video.height);
-  
+
   let d = dist(nose.x, nose.y, eye1X, eye1Y);
   
- canvas.noStroke();
- canvas.fill(0);
- canvas.ellipse (470,450,50);
- canvas.quad (400-20, 450-20, 400+30, 450-30, 400+15, 450+15, 400-25, 450+25);
- canvas.square (320-30, 425,50);
- canvas.triangle (220-40,470,220,470-50,220+40,470);
- 
-        //<-------**
-figuras(l_Wrist.x, l_Wrist.y);
-canvas.noStroke();
-canvas.fill(pincelR, pincelG, pincelB);
- if (sel_ellipse==1) {
+  if(canvasUsed == 0){
+    if (l_Wrist.y >=70){ //Cuando se está escogiendo el color no se dibuja
+      canvas0.noStroke();
+      canvas0.fill(pincelR, pincelG, pincelB);        
+      canvas0.ellipse(r_Wrist.x, r_Wrist.y, d);
+    }
+    image(canvas0, 0, 0);
+  }else{
+    if(canvasUsed == 1){
+      if (l_Wrist.y >=70){
+        canvas1.noStroke();
+        canvas1.fill(pincelR, pincelG, pincelB);        
+        canvas1.ellipse(r_Wrist.x, r_Wrist.y, d);
+      }
+      image(canvas1, 0, 0);
+    }else{
+      if(canvasUsed == 2){
+        if (l_Wrist.y >=70){
+          canvas2.noStroke();
+          canvas2.fill(pincelR, pincelG, pincelB);        
+          canvas2.ellipse(r_Wrist.x, r_Wrist.y, d);
+        }
+        image(canvas2, 0, 0);
+      }else{
+        if(canvasUsed == 3){
+          if (l_Wrist.y >=70){
+            canvas3.noStroke();
+            canvas3.fill(pincelR, pincelG, pincelB);        
+            canvas3.ellipse(r_Wrist.x, r_Wrist.y, d);
+          }
+          image(canvas3, 0, 0);
+        }
+      }
+    }
+  }
 
-canvas.ellipse(r_Wrist.x, r_Wrist.y, d);
+  canvas0.noStroke();
+  canvas0.fill(0);
+  canvas0.ellipse (470, 450, 50);
+  canvas0.quad (400-20, 450-20, 400+30, 450-30, 400+15, 450+15, 400-25, 450+25);
+  canvas0.square (320-30, 425, 50);
+  canvas0.triangle (220-40, 470, 220, 470-50, 220+40, 470);
 
-} else if (sel_quad==1) {
-  
-canvas.quad (r_Wrist.x+10-d, r_Wrist.y+10-d, r_Wrist.x+d, r_Wrist.y-d, r_Wrist.x-10+d, r_Wrist.y-10+d, r_Wrist.x+5-d, r_Wrist.y-5+d);
+  //<-------**
+  figuras(l_Wrist.x, l_Wrist.y);
+  canvas0.noStroke();
+  canvas0.fill(pincelR, pincelG, pincelB);
+  if (sel_ellipse==1) {
 
-} else if (sel_square==1){
- 
-canvas.square (r_Wrist.x, r_Wrist.y, d);
+    canvas0.ellipse(r_Wrist.x, r_Wrist.y, d);
+  } else if (sel_quad==1) {
 
-} else if (sel_triangle==1) {
-  
-canvas.triangle(r_Wrist.x-5-d, r_Wrist.y+5+d, r_Wrist.x, r_Wrist.y-d, r_Wrist.x+5+d, r_Wrist.y+5+d);
+    canvas0.quad (r_Wrist.x+10-d, r_Wrist.y+10-d, r_Wrist.x+d, r_Wrist.y-d, r_Wrist.x-10+d, r_Wrist.y-10+d, r_Wrist.x+5-d, r_Wrist.y-5+d);
+  } else if (sel_square==1) {
 
-}
+    canvas0.square (r_Wrist.x, r_Wrist.y, d);
+  } else if (sel_triangle==1) {
+
+    canvas0.triangle(r_Wrist.x-5-d, r_Wrist.y+5+d, r_Wrist.x, r_Wrist.y-d, r_Wrist.x+5+d, r_Wrist.y+5+d);
+  }
 
 
-  
-  image(canvas, 0, 0);
+
+  image(canvas0, 0, 0);
 
   image(pointer, l_Wrist.x, l_Wrist.y, d, d );  
-  
+
   boton1.show();
   boton2.show();
   boton3.show();
@@ -199,41 +231,37 @@ canvas.triangle(r_Wrist.x-5-d, r_Wrist.y+5+d, r_Wrist.x, r_Wrist.y-d, r_Wrist.x+
   botonFiltro2.showF();
   botonFiltro0.setFiltro();
   botonFiltro1.setFiltro();
-  botonFiltro2.setFiltro();  
+  botonFiltro2.setFiltro();
 }
 
-function figuras( m_izqx,  m_izqy) {
+function figuras( m_izqx, m_izqy) {
 
- if((470-50)<=m_izqx && m_izqx<= (470+50) && (450-50) <= m_izqy  && m_izqy <= (450+50) /*|| sel_ellipse ==1*/){
+  if ((470-50)<=m_izqx && m_izqx<= (470+50) && (450-50) <= m_izqy  && m_izqy <= (450+50) /*|| sel_ellipse ==1*/) {
 
-sel_ellipse=1;
-sel_quad=0;
-sel_square=0;
-sel_triangle=0;
+    sel_ellipse=1;
+    sel_quad=0;
+    sel_square=0;
+    sel_triangle=0;
+  } 
+  if ((400-25)<=m_izqx && m_izqx<= (400+30) && (450-30) <= m_izqy && m_izqy <= (450+25) /*|| sel_quad==1*/) {
 
- } 
- if((400-25)<=m_izqx && m_izqx<= (400+30) && (450-30) <= m_izqy && m_izqy <= (450+25) /*|| sel_quad==1*/){
+    sel_ellipse=0;
+    sel_quad=1;
+    sel_square=0;
+    sel_triangle=0;
+  }
+  if ((320-30)<=m_izqx && m_izqx<= (320-30+50) && 425 <= m_izqy && m_izqy<= (425+50)/* || sel_square==true*/) {
 
-sel_ellipse=0;
-sel_quad=1;
-sel_square=0;
-sel_triangle=0;
+    sel_ellipse=0;
+    sel_quad=0;
+    sel_square=1;
+    sel_triangle=0;
+  }
+  if ((220-40)<=m_izqx && m_izqx<= (220+40) && (470-50) <= m_izqy  && m_izqy <= 470/* || sel_triang==true*/) {
 
-}
- if((320-30)<=m_izqx && m_izqx<= (320-30+50) && 425 <= m_izqy && m_izqy<= (425+50)/* || sel_square==true*/){
- 
-sel_ellipse=0;
-sel_quad=0;
-sel_square=1;
-sel_triangle=0;
-
-}
- if((220-40)<=m_izqx && m_izqx<= (220+40) && (470-50) <= m_izqy  && m_izqy <= 470/* || sel_triang==true*/){
-
-sel_ellipse=0;
-sel_quad=0;
-sel_square=0;
-sel_triangle=1;
-
+    sel_ellipse=0;
+    sel_quad=0;
+    sel_square=0;
+    sel_triangle=1;
   }
 }
